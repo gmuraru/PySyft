236e24d5a168ec661b7886184fc2b57afc95f23b
George Muraru <murarugeorgec@gmail.com>
2020-03-27 04:11:12 +0200

    Add placeholder attribute

--- syft/execution/placeholder.py
+++ syft/execution/placeholder.py
@@ -25,6 +25,17 @@ class PlaceHolder(AbstractTensor):
             self.id = PlaceholderId(self.id)
         self.child = None
 
+    def __getattribute__(self, name):
+        try:
+            # Try to find the attribute in the current object
+            # we need some attributes like: instantiate, id, tags
+            response = object.__getattribute__(self, name)
+        except AttributeError:
+            child = object.__getattribute__(self, "child")
+            response = getattr(child, name)
+
+        return response
+
     def instantiate(self, tensor):
         """
         Add a tensor as a child attribute. All operations on the placeholder will be also
