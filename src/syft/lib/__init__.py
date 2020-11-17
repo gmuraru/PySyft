27f78d8f02fbbf617587a8ae2c87d90f34cc1592
George Muraru <murarugeorgec@gmail.com>
2020-11-17 19:31:50 +0200

    Add sympc simple functionality

--- src/syft/lib/__init__.py
+++ src/syft/lib/__init__.py
@@ -4,20 +4,31 @@ from ..lib.python import create_python_ast
 from ..lib.torch import create_torch_ast
 from ..lib.torchvision import create_torchvision_ast
 
+sympc_available = True
+
+try:
+    # syft relative
+    from ..lib.sympc import create_sympc_ast
+except ImportError:
+    sympc_available = False
+
 
 # now we need to load the relevant frameworks onto the node
 def create_lib_ast() -> Globals:
+    lib_ast = Globals()
 
     python_ast = create_python_ast()
-    torch_ast = create_torch_ast()
-    torchvision_ast = create_torchvision_ast()
-    # numpy_ast = create_numpy_ast()
-
-    lib_ast = Globals()
     lib_ast.add_attr(attr_name="syft", attr=python_ast.attrs["syft"])
+
+    torch_ast = create_torch_ast()
     lib_ast.add_attr(attr_name="torch", attr=torch_ast.attrs["torch"])
+
+    torchvision_ast = create_torchvision_ast()
     lib_ast.add_attr(attr_name="torchvision", attr=torchvision_ast.attrs["torchvision"])
-    # lib_ast.add_attr(attr_name="numpy", attr=numpy_ast.attrs["numpy"])
+
+    if sympc_available:
+        sympc_ast = create_sympc_ast()
+        lib_ast.add_attr(attr_name="sympc", attr=sympc_ast.attrs["sympc"])
 
     return lib_ast
 
