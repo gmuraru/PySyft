54be1acb880ba3dabd79dceb6e914db345973a89
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2020-11-24 22:59:35 +0530

    updated tests for ast

--- tests/syft/lib/sympc/tensor/ast_test.py
+++ tests/syft/lib/sympc/tensor/ast_test.py
@@ -1,66 +1,156 @@
-# stdlib
-import operator
-
 # third party
 import pytest
-from sympc.tensor import AdditiveSharingTensor
+from sympc.tensor.additive_shared import AdditiveSharingTensor
 import torch
 
 # syft absolute
 import syft as sy
 from syft.lib.sympc.session import SySession
 
-TEST_VALUES = [
-    (4, -5),
-    (torch.tensor([42, -32, 12]), 20),
-    (25, torch.tensor([32, 12, -5])),
-    (torch.tensor([15, 2353, 23, -50]), torch.tensor([123, 43, 23, -5])),
-    (4.512312, torch.tensor([123.123, 5123.321, 123.32])),
-]
 
-
-def test_ast_exception_share() -> None:
+@pytest.fixture
+def clients() -> list:
     alice = sy.VirtualMachine(name="alice")
     bob = sy.VirtualMachine(name="bob")
+    james = sy.VirtualMachine(name="james")
 
     alice_client = alice.get_client()
     bob_client = bob.get_client()
+    james_client = james.get_client()
 
-    session = SySession(parties=[alice_client, bob_client])
-
-    with pytest.raises(ValueError):
-        AdditiveSharingTensor(secret=42, session=session)
+    return [alice_client, bob_client, james_client]
 
 
-@pytest.mark.parametrize("private", [True, False])
-@pytest.mark.parametrize("operation", ["add", "sub", "mul"])
-def test_ast_operation(private: bool, operation: str) -> None:
-    alice = sy.VirtualMachine(name="alice")
-    bob = sy.VirtualMachine(name="bob")
-
-    alice_client = alice.get_client()
-    bob_client = bob.get_client()
-
-    session = SySession(parties=[alice_client, bob_client])
+def test_reconstruct(clients: list) -> None:
+    alice_client, bob_client, james_client = clients
+    session = SySession(parties=[alice_client, bob_client, james_client])
     session.setup_mpc()
 
-    for x_secret, y_secret in TEST_VALUES:
-        x = AdditiveSharingTensor(secret=x_secret, session=session)
+    x_secret = torch.Tensor([1, 2, 3, 4])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
 
-        if private:
-            y = AdditiveSharingTensor(secret=y_secret, session=session)
-        else:
-            y = y_secret
+    x = x.reconstruct()
 
-        op = getattr(operator, operation)
-        res = op(x, y)
-        res_expected = op(x_secret, y_secret)
+    assert torch.allclose(x_secret, x)
 
-        if not isinstance(res_expected, torch.Tensor):
-            res_expected = torch.tensor([res_expected])
 
-        res_expected = res_expected.float()
+def test_add(clients: list) -> None:
+    alice_client, bob_client, james_client = clients
+    session = SySession(parties=[alice_client, bob_client, james_client])
+    session.setup_mpc()
 
-        assert torch.allclose(
-            res.reconstruct(), res_expected
-        ), f"Fail for {x_secret} and {y_secret}"
+    x_secret = torch.Tensor([1, 2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x + x).reconstruct()
+    assert torch.allclose(result, (x_secret + x_secret))
+
+    x_secret = torch.Tensor([1, 2, 3])
+    y_secret = torch.Tensor([4, 5, 6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x + y).reconstruct()
+    assert torch.allclose(result, (x_secret + y_secret))
+
+    # with negative numbers
+    x_secret = torch.Tensor([1, -2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x + x).reconstruct()
+    assert torch.allclose(result, (x_secret + x_secret))
+
+    x_secret = torch.Tensor([1, -2, 3])
+    y_secret = torch.Tensor([4, 5, -6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x + y).reconstruct()
+    assert torch.allclose(result, (x_secret + y_secret))
+
+    # with constant integer
+    x_secret = torch.Tensor([1, 2, 3])
+    c = 4
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x + c).reconstruct()
+    assert torch.allclose(result, (x_secret + c))
+
+    # with constant float
+    x_secret = torch.Tensor([1, 2, 3])
+    y = 4.6
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x + c).reconstruct()
+    assert torch.allclose(result, (x_secret + y))
+
+    # test with different sessions
+    session_alternate = sy.lib.sympc.session.SySession(
+        parties=[alice_client, bob_client]
+    )
+    session_alternate.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+
+    x_session = AdditiveSharingTensor(secret=x_secret, session=session)
+    x_session_alternate = AdditiveSharingTensor(
+        secret=x_secret, session=session_alternate
+    )
+
+    with pytest.raises(ValueError):
+        result = x_session + x_session_alternate
+
+
+def test_sub(clients: list) -> None:
+    alice_client, bob_client, james_client = clients
+    session = SySession(parties=[alice_client, bob_client, james_client])
+    session.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - x).reconstruct()
+    assert torch.allclose(result, (x_secret - x_secret))
+
+    x_secret = torch.Tensor([1, 2, 3])
+    y_secret = torch.Tensor([4, 5, 6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x - y).reconstruct()
+    assert torch.allclose(result, (x_secret - y_secret))
+
+    # with negative numbers
+    x_secret = torch.Tensor([1, -2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - x).reconstruct()
+    assert torch.allclose(result, (x_secret - x_secret))
+
+    x_secret = torch.Tensor([1, -2, 3])
+    y_secret = torch.Tensor([4, 5, -6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x - y).reconstruct()
+    assert torch.allclose(result, (x_secret - y_secret))
+
+    # with constant integer
+    x_secret = torch.Tensor([1, 2, 3])
+    c = 4
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - c).reconstruct()
+    assert torch.allclose(result, (x_secret - c))
+
+    # with constant float
+    x_secret = torch.Tensor([1, 2, 3])
+    y = 4.6
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - c).reconstruct()
+    assert torch.allclose(result, (x_secret - y))
+
+    # test with different sessions
+    session_alternate = sy.lib.sympc.session.SySession(
+        parties=[alice_client, bob_client]
+    )
+    session_alternate.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+
+    x_session = AdditiveSharingTensor(secret=x_secret, session=session)
+    x_session_alternate = AdditiveSharingTensor(
+        secret=x_secret, session=session_alternate
+    )
+
+    with pytest.raises(ValueError):
+        result = x_session - x_session_alternate
